99e2640be002c0697068501b5664fc98d80e58d3
bazzmx <andrestrivera@gmail.com>
2022-05-22 12:32:06 +0200

    feat: Add event and date info to event cards in featured section

--- screens/Featured.js
+++ screens/Featured.js
@@ -7,12 +7,66 @@
  * [] Build the FOR YOU section 
  */
 import React from 'react';
-import { Text, View, StyleSheet, SafeAreaView, TextInput } from 'react-native';
+import { Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  FlatList,
+  ImageBackground
+  } from 'react-native';
 import styled from 'styled-components/native';
+import moment from 'moment';
 import { dummyData, FONTS, SIZES, COLORS, icons, images } from '../constants';
 import { McText, McIcon, McAvatar } from '../components';
 
 const Featured = ({ navigation }) => {
+
+  const _renderItem = ({item, index}) => {
+    return (
+      <View style={{
+        marginLeft: index === 0 ? 30: 20,
+        marginRight: index === dummyData.Events.length -1? 30: 0
+      }}>
+        <ImageBackground source={item.image}
+        resizeMode = 'cover'
+        borderRadius = {SIZES.radius}
+        style = {{
+          width: SIZES.width / 2 + 70,
+          height: SIZES.width / 2 + 70,
+          justifyContent: 'space-between'
+        }}
+        >
+          <View style={{
+            alignItems: 'flex-end',
+            marginHorizontal: 15,
+            marginVertical: 15,
+
+          }}>
+          <DateBox>
+            <McText body5 color={COLORS.black} style={{ opacity: 0.5,
+              letterSpacing: 2
+             }}>
+              {moment(item.startingtime).format('MMM').toUpperCase()}
+            </McText>
+            <McText h2 color={COLORS.black}>
+              {moment(item.startingtime).format('DD')}
+            </McText>
+          </DateBox>
+          </View>
+          <View style={{
+            marginLeft: 20,
+            marginBottom: 25
+          }}>
+            <McText body5 style={{opacity: 0.5}}>{item.type}</McText>
+            <McText h2>{item.title}</McText>
+          </View>
+
+        </ImageBackground>
+      </View>
+    )
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <SectionHeader>
@@ -39,6 +93,19 @@ const Featured = ({ navigation }) => {
           <McIcon source={icons.filter} size={24}/>
         </SearchView>
       </SectionSearch>
+      {/* Destacado */}
+      <SectionTitle>
+        <McText h5>DESTACADO</McText>
+        <View>
+          <FlatList
+          horizontal
+          contentContainerStyle={{}}
+          keyExtractor={(item) => 'event_' + item.id}
+          data={dummyData.Events}
+          renderItem={_renderItem}
+          ></FlatList>
+        </View>
+      </SectionTitle>
     </SafeAreaView>
   );
 };
@@ -66,6 +133,19 @@ const SearchView = styled.View`
   align-items: center;
   margin-left: 9px;
   margin-right: 15px;
+`;
+
+const SectionTitle = styled.View`
+  margin: 20px;
+`;
+
+const DateBox = styled.View`
+  width: 60px;
+  height: 60px;
+  border-radius: 15;
+  background-color: ${COLORS.white};
+  justify-content: center;
+  align-items: center;
 `;
 
 const styles = StyleSheet.create({
