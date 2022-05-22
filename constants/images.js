44e06fb30e9fc1b7a552e214ad30480e1253eeb3
bazzmx <andrestrivera@gmail.com>
2022-05-22 16:22:55 +0200

    feat: Include image background in detail view

--- constants/images.js
+++ constants/images.js
@@ -1,9 +1,15 @@
 const avatar = require("../assets/images/avatar.png");
 const event_1 = require("../assets/images/event_1.png");
 const event_2 = require("../assets/images/event_2.png");
+const event_3 = require("../assets/images/event_3.png");
+const event_4 = require("../assets/images/event_4.png");
+const event_5 = require("../assets/images/event_5.png");
 
 export default {
   avatar,
   event_1,
-  event_2
+  event_2,
+  event_3,
+  event_4,
+  event_5
 }
--- screens/EventDetail.js
+++ screens/EventDetail.js
@@ -12,19 +12,55 @@
  * [] Build the location section (google map in dark mode)
  * [] Build the fixed bottom bar
  */
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  ImageBackground,
+  Platform,
+  TouchableOpacity
+ } from 'react-native';
+import styled from 'styled-components/native';
+import moment from 'moment';
+import { LinearGradient } from 'expo-linear-gradient';
+import { dummyData, FONTS, SIZES, COLORS, icons, images } from '../constants';
+import { McText, McIcon, McAvatar } from '../components';
 
 const EventDetail = ({ navigation, route }) => {
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  useEffect(()=>{
+    let { selectedEvent } = route.params;
+    setSelectedEvent(selectedEvent);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={{ color: '#fff', fontSize: 30 }}>EventDetail</Text>
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+          backgroundColor: COLORS.black
+        }}
+        style={{
+          backgroundColor: COLORS.black
+        }}>
+          {/* Imagen de fondo */}
+          <ImageBackground
+            resizeMode='cover'
+            source={selectedEvent?.image}
+            style={{
+              width: '100%',
+              height: SIZES.height < 700 ? SIZES.height * 0.4 : SIZES.height * 0.5
+            }} 
+          ></ImageBackground>
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  ImageBackground: {
     flex: 1,
     backgroundColor: '#000',
     justifyContent: 'center',
